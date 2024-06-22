b3cd4a51505d4430c0c259dc74afdec057d8d25f
Kriti Priya <kritipriya119@gmail.com>
2024-06-22 18:16:23 +0530

    Adding main application class for Spring Boot
    creating Main Application Class Commit

---
+++ src/main/java/swiggy_oa_kriti/oa_swiggy/OaSwiggyApplication.java
@@ -0,0 +1,13 @@
+package swiggy_oa_kriti.oa_swiggy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OaSwiggyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OaSwiggyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swiggy_oa_kriti/oa_swiggy/OaSwiggyApplicationTests.java
@@ -0,0 +1,13 @@
+package swiggy_oa_kriti.oa_swiggy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OaSwiggyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
